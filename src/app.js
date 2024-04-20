80aeb541907edd8c58c696a11032254d64c1fbe1
Priti Gupta <civilstudent9900@gmail.com>
2024-04-20 13:52:38 +0530

    "Import and use user routes"

--- src/app.js
+++ src/app.js
@@ -11,9 +11,15 @@ app.use(cors({
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
-
 app.use(express.static("public"))
-
 app.use(cookieParser)
+
+//routes import
+import userRoutes from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRoutes)
+
+//https://localhost:8000/api/v1/users/register
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
 import connnectDB from "./db/index.js";
-import app from "./app.js";
+import {app} from "./app.js";
 
 dotenv.config({
   path: './env'
