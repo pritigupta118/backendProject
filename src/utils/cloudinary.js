81d2e030c03eedb03fc036401649a875f5bfc6f9
Priti Gupta <civilstudent9900@gmail.com>
2024-04-20 11:25:42 +0530

    upload file on cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLODINARY_API_KEY, 
+  api_secret: process.env.CLODINARY_API_SECRET 
+});
+
+const cloudinaryUpload = async(localPath) => {
+try {
+  if(!localPath) return null
+
+  //upload the file on cloudinary
+  const result = await cloudinary.uploader.upload(localPath);
+
+  console.log('file is uploaded on cloudinary',result.url);
+
+  fs.unlinkSync(localPath) //delete the local file after uploading to clodinary
+
+  return result;
+
+} catch (error) {
+  fs.unlinkSync(localPath)// remove the locally saved temporary file as the upload operation got failed
+  return null
+  }
+}
+
+export {cloudinaryUpload}
